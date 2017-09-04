1407569933e6950d01e2e4aa0b4b070cf3c841ae
Joshua Copeland <joshuarcopeland@gmail.com>
2017-09-04 13:09:47 -0700

    wtf

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_ecs_task_definition" "application" {
 resource "aws_ecs_service" "application" {
   name = "${var.env_name}-${var.app_name}"
   cluster = "${data.terraform_remote_state.infrastructure_state.cluster_id}"
-  task_definition = "${aws_ecs_task_definition.application.}"
+  task_definition = "${aws_ecs_task_definition.application.family}:${aws_ecs_task_definition.application.revision}"
   desired_count = "${var.service_desired}"
   iam_role = "${var.ecs_iam_role}"
 
